05ad3d573fd1510b0e25abe413d5d8130da7c109
Alexandre Amaral <alexandrecamaral22@gmail.com>
2021-10-02 14:49:56 -0300

    primeiro commit

---
+++ Prova01 Alexandre/Prova Alexandre/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Prova_Alexandre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.rdbAdicao = new System.Windows.Forms.RadioButton();
+            this.rdbSubtracao = new System.Windows.Forms.RadioButton();
+            this.rdbMultiplicacao = new System.Windows.Forms.RadioButton();
+            this.rdbDivisao = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Escolha a operação desejada: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Resultado:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(134, 36);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(124, 20);
+            this.txtNum1.TabIndex = 4;
+            this.txtNum1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtNum1_KeyUp);
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(134, 78);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(124, 20);
+            this.txtNum2.TabIndex = 5;
+            this.txtNum2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtNum2_KeyUp);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.SystemColors.Window;
+            this.txtResultado.Location = new System.Drawing.Point(134, 277);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(173, 20);
+            this.txtResultado.TabIndex = 6;
+            // 
+            // rdbAdicao
+            // 
+            this.rdbAdicao.AutoSize = true;
+            this.rdbAdicao.Location = new System.Drawing.Point(233, 131);
+            this.rdbAdicao.Name = "rdbAdicao";
+            this.rdbAdicao.Size = new System.Drawing.Size(58, 17);
+            this.rdbAdicao.TabIndex = 7;
+            this.rdbAdicao.TabStop = true;
+            this.rdbAdicao.Text = "Adição";
+            this.rdbAdicao.UseVisualStyleBackColor = true;
+            this.rdbAdicao.Click += new System.EventHandler(this.rdbAdicao_Click);
+            // 
+            // rdbSubtracao
+            // 
+            this.rdbSubtracao.AutoSize = true;
+            this.rdbSubtracao.Location = new System.Drawing.Point(233, 165);
+            this.rdbSubtracao.Name = "rdbSubtracao";
+            this.rdbSubtracao.Size = new System.Drawing.Size(74, 17);
+            this.rdbSubtracao.TabIndex = 8;
+            this.rdbSubtracao.TabStop = true;
+            this.rdbSubtracao.Text = "Subtração";
+            this.rdbSubtracao.UseVisualStyleBackColor = true;
+            this.rdbSubtracao.Click += new System.EventHandler(this.rdbSubtracao_Click);
+            // 
+            // rdbMultiplicacao
+            // 
+            this.rdbMultiplicacao.AutoSize = true;
+            this.rdbMultiplicacao.Location = new System.Drawing.Point(233, 202);
+            this.rdbMultiplicacao.Name = "rdbMultiplicacao";
+            this.rdbMultiplicacao.Size = new System.Drawing.Size(87, 17);
+            this.rdbMultiplicacao.TabIndex = 9;
+            this.rdbMultiplicacao.TabStop = true;
+            this.rdbMultiplicacao.Text = "Multiplicação";
+            this.rdbMultiplicacao.UseVisualStyleBackColor = true;
+            this.rdbMultiplicacao.Click += new System.EventHandler(this.rdbMultiplicacao_Click);
+            // 
+            // rdbDivisao
+            // 
+            this.rdbDivisao.AutoSize = true;
+            this.rdbDivisao.Location = new System.Drawing.Point(233, 238);
+            this.rdbDivisao.Name = "rdbDivisao";
+            this.rdbDivisao.Size = new System.Drawing.Size(60, 17);
+            this.rdbDivisao.TabIndex = 10;
+            this.rdbDivisao.TabStop = true;
+            this.rdbDivisao.Text = "Divisão";
+            this.rdbDivisao.UseVisualStyleBackColor = true;
+            this.rdbDivisao.Click += new System.EventHandler(this.rdbDivisao_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 337);
+            this.Controls.Add(this.rdbDivisao);
+            this.Controls.Add(this.rdbMultiplicacao);
+            this.Controls.Add(this.rdbSubtracao);
+            this.Controls.Add(this.rdbAdicao);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.RadioButton rdbAdicao;
+        private System.Windows.Forms.RadioButton rdbSubtracao;
+        private System.Windows.Forms.RadioButton rdbMultiplicacao;
+        private System.Windows.Forms.RadioButton rdbDivisao;
+    }
+}
+
---
+++ Prova01 Alexandre/Prova Alexandre/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prova_Alexandre
+{
+    public partial class Form1 : Form
+    {
+        public double num01;
+        public double num02;
+        public double res;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*Evento que apos a escrita verifica cada letra
+         se nao for um numero ele obriga a reescrever.*/
+        private void txtNum1_KeyUp(object sender, KeyEventArgs e)
+        {
+            try // previni excecoes
+            {
+                string palavra = txtNum1.Text; // pega a palavra do TextBox
+                foreach (char letra in palavra) { // percorre cada letra da palavra
+                    
+                    if (letra >= '0' && letra <= '9') { // se estiver dentro de 0 - 9 nao faz nada
+                    
+                    }
+                    else{ // caso contrario ele obriga a reescrever
+                        MessageBox.Show("So pode escrever numero.");
+                        txtNum1.Clear();
+                        txtNum1.Focus();
+                        break; // sai do foreach
+                    }
+                    
+                }                
+            }
+            catch (Exception ex){ MessageBox.Show(ex.Message); } // mostra o erro se houver uma excessao
+        }
+
+        // Mesma logica do txtNum1_KeyUp
+        private void txtNum2_KeyUp(object sender, KeyEventArgs e)
+        {
+            try 
+            {
+                string palavra = txtNum2.Text; 
+                foreach (char letra in palavra)
+                { 
+
+                    if (letra >= '0' && letra <= '9'){
+
+                    }
+                    else
+                    { 
+                        MessageBox.Show("So pode escrever numero.");
+                        txtNum2.Clear();
+                        txtNum2.Focus();
+                        break; 
+                    }
+
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); } 
+        }
+
+        private void rdbAdicao_Click(object sender, EventArgs e)
+        {
+            if (VerificaTexto()){
+
+                // pega os valores
+                num01 = Convert.ToDouble(txtNum1.Text);
+                num02 = Convert.ToDouble(txtNum2.Text);
+
+                res = num01 + num02; //calculo
+                // escreve a resposta no TextBox
+                txtResultado.Text = res.ToString(); 
+            }
+            else{ // caso nao passe pela funcao VerificaTexto()
+
+                rdbAdicao.Checked = false; // desmarca o rdb
+            }
+            
+        }
+
+        // Mesma Logica do rdbAdicao_Click so muda o calculo
+        private void rdbSubtracao_Click(object sender, EventArgs e)
+        {
+            if (VerificaTexto()){
+
+                num01 = Convert.ToDouble(txtNum1.Text);
+                num02 = Convert.ToDouble(txtNum2.Text);
+
+                res = num01 - num02;
+                txtResultado.Text = res.ToString();
+            }
+            else{
+
+                rdbSubtracao.Checked = false;
+            }
+        }
+
+        private void rdbMultiplicacao_Click(object sender, EventArgs e)
+        {
+            if (VerificaTexto()){
+
+                num01 = Convert.ToDouble(txtNum1.Text);
+                num02 = Convert.ToDouble(txtNum2.Text);
+
+                res = num01 * num02;
+                txtResultado.Text = res.ToString();
+            }
+            else{
+
+                rdbMultiplicacao.Checked = false;
+            }
+        }
+
+        private void rdbDivisao_Click(object sender, EventArgs e)
+        {
+            if (VerificaTexto())
+            {
+
+                num01 = Convert.ToDouble(txtNum1.Text);
+                num02 = Convert.ToDouble(txtNum2.Text);
+
+                res = num01 / num02;
+                txtResultado.Text = res.ToString();
+            }
+            else
+            {
+
+                rdbDivisao.Checked = false;
+            }
+        }
+
+
+        /* uma funcao para verificar se os TextBox nao estao vazios
+       para nao ter q ficar repetindo toda vez */
+        private bool VerificaTexto()
+        {
+            try
+            {
+                // verifica se o tamanho e maior que 0
+                if ((txtNum1.TextLength > 0) && (txtNum2.TextLength > 0)){
+
+                    return (true); //se for passa direto
+                }
+
+                else{ // se nao volta para os TextBox
+                    MessageBox.Show("Primeiro digite os valores");
+                    txtNum1.Focus();
+                    return (false);
+                }
+            }
+            // mostra o erro se houver uma excessao
+            catch (Exception ex) { MessageBox.Show(ex.Message); return (false); }
+        }
+    }
+}
